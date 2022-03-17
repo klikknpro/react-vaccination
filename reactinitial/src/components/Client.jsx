a541c1d0b1fa283cb795d41e8f487e5a54371007
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-17 19:56:50 +0100

    need to render vaccination change

--- reactinitial/src/components/Client.jsx
+++ reactinitial/src/components/Client.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import {React, useState} from 'react';
 import http from "axios";
 import {Button} from "@mui/material";
 
 const Client = ({client}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const changeStatus = async(pet) => {
+    setIsLoading(true);
     const response = await http.post("https://demoapi.com/api/vet/pets/",
       {
         name: pet.name,
         isVaccinated: !(pet.isVaccinated)
       });
-    console.log(response);
+    setIsLoading(false);
   };
 
   return (
@@ -19,7 +22,7 @@ const Client = ({client}) => {
         <li key={i}>
           <p>Pet's name: {pet.name}</p>
           <p>Breed: {pet.animal}</p>
-          <p>{pet.isVaccinated ? "Vaccinated" : "Not vaccinated"}
+          <p>{!isLoading ? pet.isVaccinated ? "Vaccinated" : "Not vaccinated" : "changing status..."}
             <Button onClick={() => changeStatus(pet)} color="info" variant="contained" size="small">Change status</Button>
           </p>
         </li>

e9d7ab544f95d98e4d02f0e9151bbcceb9afe3f2
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-17 19:45:13 +0100

    change status in backend

--- reactinitial/src/components/Client.jsx
+++ reactinitial/src/components/Client.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from 'react';
+import http from "axios";
+import {Button} from "@mui/material";
 
 const Client = ({client}) => {
-  console.log("from comp ", client);
+  const changeStatus = async(pet) => {
+    const response = await http.post("https://demoapi.com/api/vet/pets/",
+      {
+        name: pet.name,
+        isVaccinated: !(pet.isVaccinated)
+      });
+    console.log(response);
+  };
 
   return (
-    <div>
+    <div className="client-card">
       <h4>{client.name}</h4>
       <ul>{client.pets.map((pet, i) => (
-        <li>
+        <li key={i}>
           <p>Pet's name: {pet.name}</p>
           <p>Breed: {pet.animal}</p>
-          <p>{pet.isVaccinated ? "Vaccinated" : "Not vaccinated"}</p>
+          <p>{pet.isVaccinated ? "Vaccinated" : "Not vaccinated"}
+            <Button onClick={() => changeStatus(pet)} color="info" variant="contained" size="small">Change status</Button>
+          </p>
         </li>
       ))}</ul>
     </div>
